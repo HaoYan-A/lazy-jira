bf3bb22f500df982a9561a7a00db36858672a924
yanhao <hao.yan@unisco.com>
2025-03-29 12:06:51 +0800

    init: 初始化项目，添加 React 支持

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1>Lazy Jira</h1>
+      <p>Welcome to your Electron + React application!</p>
+    </div>
+  );
+};
+
+export default App; 
